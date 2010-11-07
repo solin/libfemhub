2ef19c11bc2b4a4cf1bac79ee4cd1e5a1f640bf3
Ondrej Certik <ondrej@certik.cz>
2010-11-06 17:38:47 -0700

    Import run on the module level

--- femhub/__init__.py
+++ femhub/__init__.py
@@ -1,3 +1,7 @@
 from domain import Domain
 from mesh import Mesh
 from plot import plotsln
+
+# TODO: we should figure out some better way to easily call all examples in
+# femhub:
+from examples.phaml_simple import run
